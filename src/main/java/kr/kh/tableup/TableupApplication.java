7f7816f68c5b74e2771bcea3c995dfd11f251b0c
hippo24 <lodenx24@gmail.com>
2025-05-04 07:07:42 +0900

    gitignore 세팅

---
+++ src/main/java/kr/kh/tableup/TableupApplication.java
@@ -0,0 +1,13 @@
+package kr.kh.tableup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kh/tableup/TableupApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kh.tableup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

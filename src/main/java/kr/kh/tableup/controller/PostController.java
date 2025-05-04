08bec5906bf64f3b354cedbd258254a64dbc56c2
hippo24 <lodenx24@gmail.com>
2025-05-04 09:14:17 +0900

    게시글 내림차순

--- src/main/java/kr/kh/tableup/controller/PostController.java
+++ src/main/java/kr/kh/tableup/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,9 +19,9 @@ public class PostController {
   PostService postService;
 
   @GetMapping("/post/list/{bo_num}")
-  public String postList(@PathVariable int bo_num) {
+  public String postList(Model model, @PathVariable int bo_num) {
     List<PostVO> list = postService.getPostList(bo_num);
-    System.out.println(list);
+    model.addAttribute("list", list);
     return "post/list";
   }
   

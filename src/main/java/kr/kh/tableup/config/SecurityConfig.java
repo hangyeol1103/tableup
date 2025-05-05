569e56d830bcaccb06ea25bb0f355e4423b9ffb5
hippo24 <lodenx24@gmail.com>
2025-05-05 10:38:05 +0900

    자동 로그인 구현

--- src/main/java/kr/kh/tableup/config/SecurityConfig.java
+++ src/main/java/kr/kh/tableup/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package kr.kh.tableup.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,18 +11,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import kr.kh.tableup.model.util.UserRole;
+import kr.kh.tableup.service.MemberDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
+  @Autowired
+  private MemberDetailService memberDetailService;
+
+  @Value("${security.rememberme.key}")
+  private String rememberMeKey;
+
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       http.csrf(csrf ->csrf.disable())
         .authorizeHttpRequests((requests) -> requests
-          .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
-          .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
-          .anyRequest().permitAll()  // 그 외 요청은 인증 필요
+        //회원과 관리자가 접근할 수 있는 url(MemberInterceptor 역할)
+        .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
+        //관리자만 접근할 수 있는 url(AdminInterceptor 역할)
+        .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+        .anyRequest().permitAll()  // 그 외 요청은 인증 필요
       )
       .formLogin((form) -> form
         .loginPage("/login")  // 커스텀 로그인 페이지 설정
@@ -28,14 +39,13 @@ public class SecurityConfig{
         .loginProcessingUrl("/login")//로그인 화면에서 로그인을 눌렀을 때 처리할 url을 지정
         .defaultSuccessUrl("/")
       )
-      /*
       //자동 로그인 처리
       .rememberMe(rm-> rm
         .userDetailsService(memberDetailService)//자동 로그인할 때 사용할 userDetailService를 추가
         .key(rememberMeKey)//키가 변경되면 기존 토큰이 무효처리
         .rememberMeCookieName("LC")//쿠키 이름
-        .tokenValiditySeconds(60 * 60 * 24 * 100)//유지 기간 : 100일
-      ) */
+        .tokenValiditySeconds(60 * 60 * 24 * 100)//유지 기간 : x일
+      )
       .logout((logout) -> logout
           .logoutUrl("/logout")//post방식
           .logoutSuccessUrl("/")

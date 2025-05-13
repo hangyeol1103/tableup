967f4b3083c9b47879edbe1f1eab57909e39a413
hippo24 <lodenx24@gmail.com>
2025-05-13 10:45:16 +0900

    VO 수정

--- src/main/java/kr/kh/tableup/model/vo/UserVO.java
+++ src/main/java/kr/kh/tableup/model/vo/UserVO.java
@@ -6,14 +6,14 @@ import lombok.Data;
 
 @Data
 public class UserVO {
-  private Integer us_no;
-  private String us_id;        
-  private String us_pw;         
-  private String us_name;       
-  private String us_phone;      
-  private String us_email;      
-  private String us_nickname;   
+  private Integer us_num;
+  private String us_id;
+  private String us_pw;
+  private String us_name;
+  private String us_phone;
+  private String us_email;
+  private String us_nickname;
   private Boolean us_sociallogin;
   private Date us_created;
-  private String us_authority;
+  private int us_state;
 }

0cbccd9fd921d467d87928cfb4cda4fd6448c23d
hippo24 <lodenx24@gmail.com>
2025-05-13 11:57:31 +0900

    관리자 회원가입 구현

--- src/main/java/kr/kh/tableup/controller/AdminController.java
+++ src/main/java/kr/kh/tableup/controller/AdminController.java
@@ -1,16 +1,43 @@
 package kr.kh.tableup.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.kh.tableup.model.vo.AdminVO;
+import kr.kh.tableup.service.AdminService;
+
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+  @Autowired
+  AdminService adminService;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
   @GetMapping("")
   public String adminHome() {
     return "admin/admin";
+  }
+
+  @GetMapping("/signup")
+  public String adminSignupForm(Model model) {
+    model.addAttribute("url", "/admin/signup");
+    return "admin/signup";
+  }
+
+  @PostMapping("/signup")
+  public String adminSignup(AdminVO admin) {
+    admin.setAd_pw(passwordEncoder.encode(admin.getAd_pw()));
+    adminService.insertAdmin(admin);
+    return "redirect:/admin";
   }
 
 }
---
+++ src/main/java/kr/kh/tableup/dao/AdminDAO.java
@@ -0,0 +1,9 @@
+package kr.kh.tableup.dao;
+
+import kr.kh.tableup.model.vo.AdminVO;
+
+public interface AdminDAO {
+
+	boolean insertAdmin(AdminVO admin);
+  
+}

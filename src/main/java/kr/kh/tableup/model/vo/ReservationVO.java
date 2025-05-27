7c179a04978edbf9b0262960bbd0cc55435e0e57
jayc722 <jaewon8469@gmail.com>
2025-05-27 14:12:45 +0900

    Update ReservationVO.java

--- src/main/java/kr/kh/tableup/model/vo/ReservationVO.java
+++ src/main/java/kr/kh/tableup/model/vo/ReservationVO.java
@@ -16,7 +16,8 @@ public class ReservationVO {
 	private LocalDateTime res_created;   // Creation time
 	private int res_state;       // Reservation state
 
-	UserVO user;
+	private UserVO user;
+	
 	private String us_name;
 	private String rt_name;
 }

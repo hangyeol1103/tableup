eb17fb4ef2123152882783dcd7dd2e3669d37cde
YDH2763 <dbehdgns12345@gmail.com>
2025-05-28 18:04:28 +0900

    12345

--- src/main/java/kr/kh/tableup/controller/ScheduleController.java
+++ src/main/java/kr/kh/tableup/controller/ScheduleController.java
@@ -74,7 +74,6 @@ public class ScheduleController {
       model.addAttribute("restimelist", restimelist);
 		}
 		
-		
 		List<LocalDate> dateList= new ArrayList<>();
 		LocalDate today = LocalDate.now();
 		for(int i=0;i<7;i++){
@@ -86,7 +85,6 @@ public class ScheduleController {
         timeList.add(startTime.plusMinutes(30 * i));
     }
 
-		
 		model.addAttribute("dateList", dateList);
 		model.addAttribute("timeList", timeList);
 		model.addAttribute("offset", offset);

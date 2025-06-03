5b8f8434c42e034a2b2d9cd6df0b53b8e3bf69bf
jayc722 <jaewon8469@gmail.com>
2025-06-03 17:27:08 +0900

    수정

--- src/main/java/kr/kh/tableup/service/ReservationService.java
+++ src/main/java/kr/kh/tableup/service/ReservationService.java
@@ -1,10 +1,7 @@
 package kr.kh.tableup.service;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
-
-import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
